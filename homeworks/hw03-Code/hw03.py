0dfcdf3fd7adddfe29ed51a16676f27e871b7dbb
chensai <chensaichochensai@foxmail.com>
2025-04-15 10:43:45 +0800

    complete hw03 but multiadder

---
+++ homeworks/hw03-Code/hw03.py
@@ -0,0 +1,341 @@
+
+""" Homework 3: Recursion """
+
+HW_SOURCE_FILE = 'hw03.py'
+
+
+#####################
+# Required Problems #
+#####################
+
+
+def integrate(f, l, r, min_interval):
+    """Return the definite integration of function f over interval 
+    [l,r], with interval length limit min_interval.
+
+    >>> abs(integrate(lambda x: x * x, 1, 2, 0.01) - (7 / 3)) < 0.001
+    True
+    >>> abs(integrate(lambda x: x, 1, 2, 0.01) - 1.5) < 0.0001
+    True
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'integrate', ['While', 'For'])
+    True
+    """
+    if l >= r:
+        return 0
+    else:
+        return 0.5*min_interval*(f(l) + f(l+min_interval)) + integrate(f, l+min_interval, r, min_interval)
+
+def contains_six(n):
+    if n < 10:
+        return n == 6
+    return (n % 10 == 6) or contains_six(n // 10)
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    5
+    >>> pingpong(8)
+    4
+    >>> pingpong(15)
+    3
+    >>> pingpong(21)
+    5
+    >>> pingpong(22)
+    6
+    >>> pingpong(30)
+    10
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    -1
+    >>> pingpong(72)
+    -2
+    >>> pingpong(100)
+    6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    def helper(x, direction, count):
+        if x == n:
+            return count
+        elif x % 6 == 0 or contains_six(x):
+            return helper(x+1, -direction, count-direction)
+        else:
+            return helper(x+1, direction, count+direction)
+    return helper(1, 1, 1)
+
+
+
+def balanced(s):
+    """Returns whether the given parentheses sequence s is balanced.
+    >>> balanced('()')
+    True
+    >>> balanced(')')
+    False
+    >>> balanced('(())')
+    True
+    >>> balanced('()()')
+    True
+    >>> balanced('()())')
+    False
+    >>> balanced('()(()')
+    False
+    """
+
+    def divide(s, k):
+        """Divide the given parentheses sequence s into two parts at position k.
+        >>> left, right = divide('()()', 2)
+        >>> left
+        '()'
+        >>> right
+        '()'
+        >>> left, right = divide('(())()', 4)
+        >>> left
+        '(())'
+        >>> right
+        '()'
+        >>> left, right = divide('(())()', 6)
+        >>> left
+        '(())()'
+        >>> right
+        ''
+        """
+        return (s[:k], s[k:])
+
+    def peel(s):
+        """Peel off the leftmost and rightmost parentheses in s to obtain the
+        internal part of the parentheses sequence.
+        >>> peel('(())')
+        '()'
+        >>> peel('()')
+        ''
+        >>> peel('))((')
+        ')('
+        """
+        return s[1:-1]
+
+    def match(s):
+        """Returns whether the leftmost and the rightmost parentheses in s match.
+        >>> match('()')
+        True
+        >>> match('()()')
+        True
+        >>> match('()))')
+        True
+        >>> match('))')
+        False
+        >>> match(')())')
+        False
+        """
+        return s[0] == '(' and s[-1] == ')'
+
+    if s == '':
+        return True
+    elif len(s) % 2 == 1:
+        return False
+    elif not match(s):
+        return False
+    elif balanced(peel(s)):
+        return True
+    else:
+        for k in range(2, len(s), 2):
+            left, right = divide(s, k)
+            if balanced(left) and balanced(right):
+                return True
+        return False
+
+
+def count_change(total, money):
+    """Return the number of ways to make change for total,
+    under the currency system described by money.
+
+    >>> def chinese_yuan(ith):
+    ...     if ith == 1:
+    ...         return 1
+    ...     if ith == 2:
+    ...         return 5
+    ...     if ith == 3:
+    ...         return 10
+    ...     if ith == 4:
+    ...         return 20
+    ...     if ith == 5:
+    ...         return 50
+    ...     if ith == 6:
+    ...         return 100
+    >>> def us_cent(ith):
+    ...     if ith == 1:
+    ...         return 1
+    ...     if ith == 2:
+    ...         return 5
+    ...     if ith == 3:
+    ...         return 10
+    ...     if ith == 4:
+    ...         return 25
+    >>> count_change(15, chinese_yuan)
+    6
+    >>> count_change(49, chinese_yuan)
+    44
+    >>> count_change(49, us_cent)
+    39
+    >>> count_change(49, lambda x: 2 ** (x - 1))
+    692
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    def max_num_coins(n):
+        if money(n) is None:
+            return n - 1
+        if total < money(n):
+            return n - 1
+        return max_num_coins(n + 1)
+
+    
+    def count_ways(total, max_coin):
+        if total < 0:
+            return 0
+        if max_coin == 0:
+            return 0
+        if total == 0:
+            return 1
+        return count_ways(total - money(max_coin), max_coin) + count_ways(total, max_coin - 1)
+    
+    return count_ways(total, max_num_coins(1))
+
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    if n == 1:
+        print_move(start, end)
+    else:
+        other = 6 - start -end
+        move_stack(n - 1, start, other)
+        move_stack(1, start, end)
+        move_stack(n - 1, other, end)
+
+
+
+def multiadder(n):
+    """Return a function that takes N arguments, one at a time, and adds them.
+    >>> f = multiadder(3)
+    >>> f(5)(6)(7) # 5 + 6 + 7
+    18
+    >>> multiadder(1)(5)
+    5
+    >>> multiadder(2)(5)(6) # 5 + 6
+    11
+    >>> multiadder(4)(5)(6)(7)(8) # 5 + 6 + 7 + 8
+    26
+    >>> from construct_check import check
+    >>> # Make sure multiadder is a pure function.
+    >>> check(HW_SOURCE_FILE, 'multiadder',
+    ...       ['Nonlocal', 'Global'])
+    True
+    """
+    def adder(x):
+        
+            
+
+
+
+##########################
+# Just for fun Questions #
+##########################
+
+
+from operator import sub, mul
+
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
+
+
+
+def Y(f): return (lambda x: x(x))(lambda x: f(lambda z: x(x)(z)))
+def fib_maker(f): return lambda r: 'YOUR_EXPRESSION_HERE'
+def number_of_six_maker(f): return lambda r: 'YOUR_EXPRESSION_HERE'
+
+
+my_fib = Y(fib_maker)
+my_number_of_six = Y(number_of_six_maker)
+
+# This code sets up doctests for my_fib and my_number_of_six.
+
+my_fib.__name__ = 'my_fib'
+my_fib.__doc__ = """Given n, returns the nth Fibonacci nuimber.
+
+>>> my_fib(0)
+0
+>>> my_fib(1)
+1
+>>> my_fib(2)
+1
+>>> my_fib(3)
+2
+>>> my_fib(4)
+3
+>>> my_fib(5)
+5
+"""
+
+my_number_of_six.__name__ = 'my_number_of_six'
+my_number_of_six.__doc__ = """Return the number of 6 in each digit of a positive integer n.
+
+>>> my_number_of_six(666)
+3
+>>> my_number_of_six(123456)
+1
+"""
+
+    
